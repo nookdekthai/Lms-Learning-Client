f59434ad6197afc060c01ac07b827e1cfc2a6e7d
21 Tech Enterprise <21techenterprise@Macintosh.local>
2023-09-13 01:51:39 +0700

    final

--- app/components/Course/CourseDetails.tsx
+++ app/components/Course/CourseDetails.tsx
@@ -246,9 +246,7 @@ const CourseDetails = ({
                 )}
               </div>
               <br />
-              <p className="pb-1 text-black dark:text-white">
-                • Source code included
-              </p>
+            
               <p className="pb-1 text-black dark:text-white">
                 • Full lifetime access
               </p>
