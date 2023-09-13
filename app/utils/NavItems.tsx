bb3e95db20f739d1c09f3e72747d17936633111a
21 Tech Enterprise <21techenterprise@Macintosh.local>
2023-09-13 19:44:31 +0700

    final

--- app/utils/NavItems.tsx
+++ app/utils/NavItems.tsx
@@ -37,11 +37,10 @@ const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
           navItemsData.map((i, index) => (
             <Link href={`${i.url}`} key={index} passHref>
               <span
-                className={`${
-                  activeItem === index
-                    ? "dark:text-[#37a39a] text-[crimson]"
-                    : "dark:text-white text-black"
-                } text-[18px] px-6 font-Poppins font-[400]`}
+                className={`${activeItem === index
+                  ? "dark:text-[#37a39a] text-[crimson]"
+                  : "dark:text-white text-black"
+                  } text-[18px] px-6 font-Poppins font-[400]`}
               >
                 {i.name}
               </span>
@@ -50,28 +49,27 @@ const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
       </div>
       {isMobile && (
         <div className="800px:hidden mt-5">
-             <div className="w-full text-center py-6">
+          <div className="w-full text-center py-6">
             <Link href={"/"} passHref>
               <span
                 className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
               >ELearning</span>
             </Link>
           </div>
-            {navItemsData &&
-              navItemsData.map((i, index) => (
-                <Link href="/" passHref key={index}>
-                  <span
-                    className={`${
-                      activeItem === index
-                        ? "dark:text-[#37a39a] text-[crimson]"
-                        : "dark:text-white text-black"
+          {navItemsData &&
+            navItemsData.map((i, index) => (
+              <Link href={`${i.url}`} passHref key={index}>
+                <span
+                  className={`${activeItem === index
+                    ? "dark:text-[#37a39a] text-[crimson]"
+                    : "dark:text-white text-black"
                     } block py-5 text-[18px] px-6 font-Poppins font-[400]`}
-                  >
-                    {i.name}
-                  </span>
-                </Link>
-              ))}
-          </div>
+                >
+                  {i.name}
+                </span>
+              </Link>
+            ))}
+        </div>
       )}
     </>
   );
