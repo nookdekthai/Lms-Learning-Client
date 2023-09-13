6bc07d3976eef9cfc6983f9e383e328ac165b686
21 Tech Enterprise <21techenterprise@Macintosh.local>
2023-09-13 22:47:40 +0700

    final

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -11,50 +11,43 @@ const About = () => {
       
       <br />
       <div className="w-[95%] 800px:w-[85%] m-auto">
-        <p className="text-[18px] font-Poppins">
-          เราคือผู้เชี่ยวชาญทางด้าน ISO
+        <p className="text-[20px] text-white ">
+           เราจัดหา เเละให้คำเเนะนำในสิ่งที่ดีที่สุด เพื่อสิ่งที่ถูกต้อง
           <br />
+          <br/>
+          <p className="text-[25px] text-gradient ">
+            Vision
+          </p>
           <br />
-          As the founder and CEO of Becodemy, I know firsthand the challenges
-          that come with learning and growing in the programming industry.
-          That&apos;s why I created Becodemy &ndash; to provide new programmers
-          with the resources and support they need to succeed.
+          เราจะมุ่งมั่นพัฒนาผู้ประกอบการเพื่อเพิ่มศักยภาพของบุคคลากรเเละองกรณ์ให้มีความรู้เท่าทัน
+          ภัยคุกคามเทียบเท่ากับองค์กรระดับโลก
+         
           <br />
+          <br/>
+          <p className="text-[25px] text-gradient ">
+            Mission
+          </p>
           <br />
-          Our YouTube channel is a treasure trove of informative videos on
-          everything from programming basics to advanced techniques. But that&apos;s
-          just the beginning. Our affordable courses are designed to give you
-          the high-quality education you need to succeed in the industry,
-          without breaking the bank.
+          เราจะให้บริการลูกค้า ด้วยทีมงานที่มีความรู้ ความสามารถ เเละเชี่ยวชาญเฉพาะด้าน
+          ทั้งระบบจัดการเเละระบบคุณภาพตามมาตรฐานสากล โดยยึดมั่นในจรรยาบรรณวิชาชีพ
           <br />
+          <br/>
+          <p className="text-[25px] text-gradient ">
+            Value
+          </p>
           <br />
-          At Becodemy, we believe that price should never be a barrier to
+           we believe that price should never be a barrier to
           achieving your dreams. That&apos;s why our courses are priced low
           &ndash; so that anyone, regardless of their financial situation, can
           access the tools and knowledge they need to succeed.
           <br />
-          <br />
-          But Becodemy is more than just a community &ndash; we&apos;re a
-          family. Our supportive community of like-minded individuals is here
-          to help you every step of the way, whether you&apos;re just starting
-          out or looking to take your skills to the next level.
-          <br />
-          <br />
-          With Becodemy by your side, there&apos;s nothing standing between you
-          and your dream job. Our courses and community will provide you with
-          the guidance, support, and motivation you need to unleash your full
-          potential and become a skilled programmer.
-          <br />
-          <br />
-          So what are you waiting for? Join the Becodemy family today and let&apos;s
-          conquer the programming industry together! With our affordable
-          courses, informative videos, and supportive community, the sky&apos;s
-          the limit.
+       
+       
         </p>
         <br />
-        <span className="text-[22px]">Shahriarsajeeb&apos;s</span>
+        <span className="text-[22px]">Mr. </span>
         <h5 className="text-[18px] font-Poppins">
-          Founder and CEO of Becodemy
+          Founder and CEO of Foodtek-ftc
         </h5>
         <br />
         <br />
