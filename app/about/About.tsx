2546afe780f5e407c12aac85b3d600ca38fecd3e
21 Tech Enterprise <21techenterprise@Macintosh.local>
2023-09-13 23:05:21 +0700

    final

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -11,38 +11,37 @@ const About = () => {
       
       <br />
       <div className="w-[95%] 800px:w-[85%] m-auto">
-        <p className="text-[20px] text-white ">
-           เราจัดหา เเละให้คำเเนะนำในสิ่งที่ดีที่สุด เพื่อสิ่งที่ถูกต้อง
+        <p className="text-[18px] font-Poppins">
+        เราจัดหา เเละให้คำเเนะนำในสิ่งที่ดีที่สุด เพื่อสิ่งที่ถูกต้อง
           <br />
-          <br/>
-          <p className="text-[25px] text-gradient ">
-            Vision
-          </p>
+          <br  />
+          <span className="text-gradient text-[25px]">Vision</span>
+          <br />
           <br />
           เราจะมุ่งมั่นพัฒนาผู้ประกอบการเพื่อเพิ่มศักยภาพของบุคคลากรเเละองกรณ์ให้มีความรู้เท่าทัน
           ภัยคุกคามเทียบเท่ากับองค์กรระดับโลก
-         
           <br />
-          <br/>
-          <p className="text-[25px] text-gradient ">
-            Mission
-          </p>
+          <br />
+          <span className="text-gradient text-[25px]">Mission</span>
+
+          <br />
           <br />
           เราจะให้บริการลูกค้า ด้วยทีมงานที่มีความรู้ ความสามารถ เเละเชี่ยวชาญเฉพาะด้าน
           ทั้งระบบจัดการเเละระบบคุณภาพตามมาตรฐานสากล โดยยึดมั่นในจรรยาบรรณวิชาชีพ
           <br />
+          <br />
+          <span className="text-gradient text-[25px]">Value</span>
+
+          <br />
+          <br />
+          Fair  : ความเที่ยงธรรม , ยุติธรรม เเละโปร่งใส
           <br/>
-          <p className="text-[25px] text-gradient ">
-            Value
-          </p>
-          <br />
-           we believe that price should never be a barrier to
-          achieving your dreams. That&apos;s why our courses are priced low
-          &ndash; so that anyone, regardless of their financial situation, can
-          access the tools and knowledge they need to succeed.
-          <br />
-       
-       
+          <br/>
+          Trust : น่าเชื่อถือ เเละไว้วางใจ 
+          <br/>
+          <br/>
+          Customer orientd : มุ่งเน้นความต้องการลูกค้าเป็นหลัก
+          
         </p>
         <br />
         <span className="text-[22px]">Mr. </span>
