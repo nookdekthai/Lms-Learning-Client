e04ed4716a71978189876dd8330ab5b46a0c7ccb
21 Tech Enterprise <21techenterprise@Macintosh.local>
2023-09-13 18:42:52 +0700

    final

--- app/components/Route/Hero.tsx
+++ app/components/Route/Hero.tsx
@@ -41,9 +41,8 @@ const Hero: FC<Props> = (props) => {
         />
       </div>
       <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
-        <h2 className="dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[50px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[60%] 1100px:w-[78%]">
+        <h2 className="dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[70px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[60%] 1100px:w-[78%]">
           {data?.layout?.banner?.title}
-         Welcome to our academy
         </h2>
         <br />
         <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]">
@@ -84,7 +83,7 @@ const Hero: FC<Props> = (props) => {
             className="rounded-full ml-[-20px]"
           />
           <p className="font-Josefin dark:text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[18px] font-[600]">
-            500 + ผู้สมัครเรียนที่เชื่อในเรา{" "}
+            500K+ People already trusted us.{" "}
             <Link
               href="/courses"
               className="dark:text-[#46e256] text-[crimson]"
